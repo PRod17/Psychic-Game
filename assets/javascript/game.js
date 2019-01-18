efec8e35a6cf9e37288f3eb6c7535e9102731806
Paul Rodriguez <prodcapital@yahoo.com>
2019-01-18 11:48:01 -0700

    repush

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,67 @@
-function generateAlphabet(){
+function generateAlphabet() {
     var arr = [];
-    for(var i = 97; i <= 122; i++){
-      arr.push(String.fromCharCode(i));
+    for (var i = 97; i <= 122; i++) {
+        arr.push(String.fromCharCode(i));
     }
     return arr;
-  }
-  
-  var alphabet = generateAlphabet();
-  var $alreadyGuessed = document.getElementById("already-guessed");
-  console.log($alreadyGuessed);
-  var guessesAllowed= document.getElementById("remaining-guesses");
-  console.log(guessesAllowed);
-  
-  var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-  
-  
-  console.log("Computer Guess:", computerGuess);
-  var alreadyGuessedArr = [];
-  
-  document.onkeyup = function(event) {
+}
+
+var wins = 0;
+var losses = 0;
+var guessesAllowed = 9;
+
+
+
+var alphabet = generateAlphabet();
+var $alreadyGuessed = document.getElementById("already-guessed");
+console.log($alreadyGuessed);
+var $guessesAllowed = document.getElementById("remaining-guesses");
+console.log($guessesAllowed);
+var $winsText = document.getElementById("wins-text");
+console.log($winsText);
+var $lossesText = document.getElementById("losses-text");
+console.log($lossesText);
+
+
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+
+console.log("Computer Guess:", computerGuess);
+var alreadyGuessedArr = [];
+
+document.onkeyup = function (event) {
     var userGuess = event.key;
     alreadyGuessedArr.push(userGuess);
-    
-  
+
     $alreadyGuessed.textContent = alreadyGuessedArr.join(",   ");
-  }
 
+    $winsText.textContent = wins;
+    $lossesText.textContent = losses;
+    if (alreadyGuessedArr.length <= 9) {
+        if (userGuess === computerGuess) {
+            wins++;
+        
 
-  var wins = 0;
-  var losses = 0;
-  var guessesAllowed = 9;
+            computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+            console.log("Computer Guess:" + computerGuess);
+            alreadyGuessedArr = [];
+        }
+        else {
+            guessesAllowed--;
+            $guessesAllowed.textContent = guessesAllowed
+            console.log("guessesAllowed", guessesAllowed);
+            
+        }
 
+        if (guessesAllowed === 0) {
+            losses++;
+            guessesAllowed = 9;
+            console.log("Computer Guess:" + computerGuess);
+            alreadyGuessedArr = []
 
-    if (userGuess === computerGuess) {
-    wins++;
+        }
+
+    }
+    else { alreadyGuessedArr = [] }
+
 }
-    if (userGuess !== computerGuess) {
-    losses++;
-}
-    if (userGuess) {
-    guessesAllowed--;
-}1
